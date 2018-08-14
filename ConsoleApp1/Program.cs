546d4bc6fcdfb46f5ae048e79d836040aa3dc181
Faraway <Faraway@DESKTOP-B4VKRDT>
2018-08-14 23:38:57 +0800

    First

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var query = from n in arr
+                        where n > 1 && n < 6
+                        group n by n % 2;
+
+            foreach (var element in query)
+                Console.WriteLine(element);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index() => "hello from store.index";
+
+        public string Browse()
+        {
+            return "hello from store.browse";
+        }
+
+        public string Detail() => "hello from store.detail";
+    }
+}
